f955584caf46bec2e691d9b98101531d166c22c5
Dale Black <djblack@uci.edu>
2023-11-23 09:55:38 -0800

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,21 +5,25 @@ using Test
 @testset "1D" begin
     ns = [10, 100]
     for n in ns
-        x = rand(Bool, n)
-        x_inv = .!x
-        test = pytransform(x)
-        answer = distance_transform(feature_transform(x_inv))
-        @test test ≈ answer
+        for i in 1:100
+            x = rand(Bool, n)
+            x_inv = .!x
+            test = pytransform(x)
+            answer = distance_transform(feature_transform(x_inv))
+            @test test ≈ answer
+        end
     end
 end
 
 @testset "2D" begin
     ns = [10, 100]
     for n in ns
-        x = rand(Bool, n, n)
-        x_inv = .!x
-        test = pytransform(x)
-        answer = distance_transform(feature_transform(x_inv))
-        @test test ≈ answer
+        for i in 1:100
+            x = rand(Bool, n, n)
+            x_inv = .!x
+            test = pytransform(x)
+            answer = distance_transform(feature_transform(x_inv))
+            @test test ≈ answer
+        end
     end
 end
